c0ddc30aaf53fd626241a5bbb69694613cbb7fa1
uday160386 <vuktales@gmail.com>
2024-08-25 15:02:15 +0800

    minor changes to ui

--- scripts/upload_to_blob.py
+++ scripts/upload_to_blob.py
@@ -1,6 +1,6 @@
 from azure.storage.blob import BlobClient, ContentSettings
 
-connection_string = "DefaultEndpointsProtocol=https;AccountName=asrblobstorage;AccountKey=VmCgMfZS1QJa7WuU6PwN4nhnIrnYfskgFG9mNqKcG7yZAwtPwrgX29u3fIriJVPDTJE0GMmJRC8D+ASttEjzbg==;EndpointSuffix=core.windows.net"
+connection_string = "<Connection String>"
 def upload_wav_file_content_to_Blob(blobname, wav_content):
     blob = BlobClient.from_connection_string(conn_str=connection_string, container_name="asrcontainer", blob_name=blobname)
     blob.upload_blob(wav_content)
